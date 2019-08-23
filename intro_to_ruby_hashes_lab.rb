bf9c5c31545b941c04ddbec41d94a8dc012b0d92
Daniel Park <parkdk96@gmail.com>
2019-08-23 10:16:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,6 @@ def monopoly_with_second_tier
   # complexity.
   monopoly = {
     :railroads => {:pieces => 4}
-    
   }
 end
 
@@ -20,6 +19,9 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = {
+    :railroads => {:pieces => 4, :rent_in_dollars => {}}
+  }
 end
 
 def monopoly_with_fourth_tier
